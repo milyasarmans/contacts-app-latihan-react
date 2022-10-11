b6a088ac448cffe3d37f853ebfc3e3ffec948e08
milyasarmans <milyasarmans13@gmail.com>
2022-10-11 08:43:56 +0700

    Latihan Fundamental React

--- 1.contacts-app/src/components/ContactApp.js
+++ 1.contacts-app/src/components/ContactApp.js
@@ -4,7 +4,9 @@ import Navigation from './Navigation';
 import HomePage from '../pages/HomePage';
 import AddPage from '../pages/AddPage';
 import RegisterPage from '../pages/RegisterPage';
- 
+import LoginPage from '../pages/LoginPage';
+import { getUserLogged, putAccessToken } from '../utils/api';
+import { LocaleProvider } from '../contexts/LocaleContext';
  
 class ContactApp extends React.Component {
   constructor(props) {
@@ -12,40 +14,99 @@ class ContactApp extends React.Component {
  
     this.state = {
       authedUser: null,
+      initializing: true,
+      localeContext: {
+        locale: localStorage.getItem('locale') || 'id',
+        toggleLocale: () => {
+          this.setState((prevState) => {
+            const newLocale = prevState.localeContext.locale === 'id' ? 'en' : 'id';
+            localStorage.setItem('locale', newLocale);
+            return {
+              localeContext: {
+                ...prevState.localeContext,
+                locale: newLocale
+              }
+            }
+          });
+        }
+      }
     };
+ 
+    this.onLoginSuccess = this.onLoginSuccess.bind(this);
+    this.onLogout = this.onLogout.bind(this);
+  }
+
+  async componentDidMount() {
+    const { data } = await getUserLogged();
+ 
+    this.setState(() => {
+      return {
+        authedUser: data,
+        initializing: false
+      }
+    });
+  }
+ 
+  onLogout() {
+    this.setState(() => {
+      return {
+        authedUser: null
+      }
+    });
+    putAccessToken('');
+  }
+
+  async onLoginSuccess({ accessToken }) {
+    putAccessToken(accessToken);
+    const { data } = await getUserLogged();
+ 
+    this.setState(() => {
+      return {
+        authedUser: data,
+      };
+    });
   }
  
   render() {
+    if (this.state.initializing) {
+      return null;
+    }
+    
  
     if (this.state.authedUser === null) {
       return (
+        <LocaleProvider value={this.state.localeContext}>
         <div className='contact-app'>
           <header className='contact-app__header'>
             <h1>Aplikasi Kontak</h1>
           </header>
           <main>
             <Routes>
-              <Route path="/*" element={<p>Halaman Login</p>} />
+              <Route path="/*" element={<LoginPage loginSuccess={this.onLoginSuccess} />} />
               <Route path="/register" element={<RegisterPage />} />
             </Routes>
           </main>
         </div>
-      )
+        </LocaleProvider>
+      );
     }
  
     return (
-      <div className="contact-app">
-        <header className='contact-app__header'>
-          <h1>Aplikasi Kontak</h1>
-          <Navigation />
-        </header>
-        <main>
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/add" element={<AddPage />} />
-          </Routes>
-        </main>
-      </div>
+      <LocaleProvider value={this.state.localeContext}>
+        <div className="contact-app">
+          <header className='contact-app__header'>
+            <h1>{this.state.localeContext.locale === 'id' ? 'Aplikasi Kontak' : 'Contacts App'}</h1>
+            <Navigation logout={this.onLogout} name={this.state.authedUser.name} />
+          </header>
+          <main>
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/add" element={<AddPage />} />
+              <Route path="/register" element={<RegisterPage />} />
+            </Routes>
+          </main>
+        </div>
+      </LocaleProvider>
     );
   }
 }
--- 1.contacts-app/src/pages/HomePage.js
+++ 1.contacts-app/src/pages/HomePage.js
@@ -2,67 +2,89 @@ import React from 'react';
 import { useSearchParams } from 'react-router-dom';
 import ContactList from '../components/ContactList';
 import SearchBar from '../components/SearchBar';
-import { deleteContact, getContacts } from '../utils/data';
- 
+import { deleteContact, getContacts } from '../utils/api';
+import { LocaleConsumer } from '../contexts/LocaleContext';
+
 function HomePageWrapper() {
   const [searchParams, setSearchParams] = useSearchParams();
+
   const keyword = searchParams.get('keyword');
+
   function changeSearchParams(keyword) {
     setSearchParams({ keyword });
   }
- 
+
   return <HomePage defaultKeyword={keyword} keywordChange={changeSearchParams} />
 }
- 
+
 class HomePage extends React.Component {
-    constructor(props) {
-      super(props);
-   
-      this.state = {
-        contacts: getContacts(),
-        keyword: props.defaultKeyword || '',
-      }
-   
-      this.onDeleteHandler = this.onDeleteHandler.bind(this);
-      this.onKeywordChangeHandler = this.onKeywordChangeHandler.bind(this);
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      contacts: [],
+      keyword: props.defaultKeyword || '',
     }
-   
-    onDeleteHandler(id) {
-      deleteContact(id);
-   
-      // update the contact state from data.js
-      this.setState(() => {
-        return {
-          contacts: getContacts(),
+
+    this.onDeleteHandler = this.onDeleteHandler.bind(this);
+    this.onKeywordChangeHandler = this.onKeywordChangeHandler.bind(this);
+  }
+
+  async componentDidMount() {
+    const { data } = await getContacts();
+    
+    this.setState(() => {
+      return {
+        contacts: data
+      }
+    })
+  }
+
+  async onDeleteHandler(id) {
+    await deleteContact(id);
+
+    // update the contact state from api.js
+    const { data  } = await getContacts();
+    this.setState(() => {
+      return {
+        contacts: data,
+      }
+    });
+  }
+
+  onKeywordChangeHandler(keyword) {
+    this.setState(() => {
+      return {
+        keyword,
+      }
+    });
+
+    this.props.keywordChange(keyword);
+  }
+
+  render() {
+    const contacts = this.state.contacts.filter((contact) => {
+      return contact.name.toLowerCase().includes(
+        this.state.keyword.toLowerCase()
+      );
+    });
+ 
+    return (
+      <LocaleConsumer>
+        {
+          ({ locale }) => {
+            return (
+              <section>
+                <SearchBar keyword={this.state.keyword} keywordChange={this.onKeywordChangeHandler} />
+                <h2>{locale === 'id' ? 'Daftar Kontak' : 'Contacts List'}</h2>
+                <ContactList contacts={contacts} onDelete={this.onDeleteHandler} />
+              </section>
+            )
+          }
         }
-      });
-    }
-   
-    onKeywordChangeHandler(keyword) {
-        this.setState(() => {
-        return {
-            keyword,
-        }
-        });
+      </LocaleConsumer>
+    )
+  }
+}
  
-        this.props.keywordChange(keyword);
-      }
- 
-    render() {
-        const contacts = this.state.contacts.filter((contact) => {
-          return contact.name.toLowerCase().includes(
-            this.state.keyword.toLowerCase()
-          );
-        });
-        
-        return (
-          <section>
-            <SearchBar keyword={this.state.keyword} keywordChange={this.onKeywordChangeHandler} />
-            <h2>Daftar Kontak</h2>
-            <ContactList contacts={contacts} onDelete={this.onDeleteHandler} />
-          </section>
-        )
-      }
-    }
- 
-    export default HomePageWrapper;
+export default HomePageWrapper;
